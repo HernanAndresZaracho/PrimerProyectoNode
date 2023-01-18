64d3d80fa88ee70a14ed4cbacdc1e0ca257e807a
HernanAndresZaracho <hernanandreszaracho@gmail.com>
2023-01-18 02:58:06 -0300

    Agregamos Ejercicio2 resuelto

---
+++ Ejercicio2.js
@@ -0,0 +1,12 @@
+let edad = 26;
+let nombre = "Hernan Andres Zaracho";
+let desarrollador = true;
+const cumple = new Date(1996, 5+1, 17)
+const libro = {
+    titulo: "El Gaucho Martín Fierro",
+    autor: "José Hernández",
+    fecha: new Date(1872, 10+1, 28),
+    url: "https://es.wikipedia.org/wiki/El_Gaucho_Mart%C3%ADn_Fierro"
+}
+const datos = [edad, nombre, desarrollador, cumple, libro]
+console.log('datos :>> ', datos);
